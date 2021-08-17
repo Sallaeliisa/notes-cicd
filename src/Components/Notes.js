d4c51038e65d27a55bcdefe35bc474202e17c519
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-08-17 21:34:30 +0300

    test commit

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
